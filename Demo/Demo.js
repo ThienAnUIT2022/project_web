44516f91d56fcd9fe8d1721cc127567cefdefc55
Tuan Anh <117835711+ViWjin@users.noreply.github.com>
2024-04-09 14:58:16 +0700

    Add files via upload

--- Demo/Demo.js
+++ Demo/Demo.js
@@ -1,36 +1,38 @@
-$("#form-login").validate({
-    rules:{
-        username:{
-            minlength: 6,
-        },
-        password:{
-            minlength: 6,
-        }
-    },
-    messages: {
-        username:{
-            required: "Please enter your Username",
-            minlength: jQuery.validator.format("Please, Username at least 6 characters")
-        },
-        password:{
-            required: "Please enter your password",
-            minlength: jQuery.validator.format("Please, password at least 6 characters")
-        }
-    },
-    submitHandler: function(form) {
-        form.submit();
-    }
-});
-
 $(document).ready(function(){
+  $.when(
+    // Function to toggle password visibility
     $('#eye').click(function(){
         $(this).toggleClass('Open');
         $(this).children('i').toggleClass('bi-eye-slash-fill bi-eye-fill');
         if($(this).hasClass('Open')){
-            $(this).prev().attr('type', 'text');
+          $('#password').attr('type', 'text');
         } else{
-            $($(this).prev()).attr('type','password');
+          $($('#password')).attr('type','password');
         }
-    });
-});
+    }),
 
+    // Function to validate form on submit
+    $('#submit-login').click(function(){
+      $("#form-login").validate({
+        rules:{
+          username:{
+            minlength: 6,
+          },
+          password:{
+            minlength: 6,
+          }
+        },
+        messages: {
+          username:{
+            required: "Please enter your Username",
+            minlength: jQuery.validator.format("Please, Username at least 6 characters")
+          },
+          password:{
+            required: "Please enter your password",
+            minlength: jQuery.validator.format("Please, password at least 6 characters")
+          }
+        },
+      });
+    })
+  );
+});
